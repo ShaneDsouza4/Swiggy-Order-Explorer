015ef94f091889c25fa87a143bac7911ba67dbaf
ShaneDsouza4 <shane.json96@gmail.com>
2024-09-11 23:51:10 -0400

    Nested parent child div structure using React.createElement()

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-const heading = React.createElement(
-    "h1", 
-    {id: "heading"}, 
-    "Hello World from React!")
+/* 
+Nested Structurre in React 
+
+<div id="parent">
+    <div class="child1">
+        <h1 id="h1">Heading 1</h1>
+        <h1 id="h2">Heading 2</h1>
+    </div>
+    <div class="child2">
+        <h1 id="h1">Heading 1</h1>
+        <h1 id="h2">Heading 2</h1>
+    </div>
+</div>
+
+*/
+
+const parent = React.createElement(
+    "div", 
+    {id: "parent"}, [
+        React.createElement( "div", {id:"child1"}, [
+            React.createElement( "h1", {}, "Heading 1"),
+            React.createElement( "h2", {}, "Heading 2")
+        ]),
+        React.createElement( "div", {id:"child2"}, [
+            React.createElement( "h1", {}, "Heading 1"),
+            React.createElement( "h2", {}, "Heading 2")
+        ])
+    ]
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading); 
+root.render(parent); 
