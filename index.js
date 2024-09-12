9cc873b9aa3c33a2e78c4f298904f3dc1e92f1d3
ShaneDsouza4 <shane.json96@gmail.com>
2024-09-11 22:32:38 -0400

    Hello World Injected via React

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const heading = document.createElement("h1");
-heading.innerHTML = "Hello World from Vanilla JavaScript"
-
-const root =  document.getElementById('root')
-root.appendChild(heading);
+const heading = React.createElement("h1", {}, "Hello World from React!")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
