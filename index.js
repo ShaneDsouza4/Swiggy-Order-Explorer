c43222adf51f0b8f84c5d51804a4fe07f40dd820
ShaneDsouza4 <shane.json96@gmail.com>
2024-09-13 15:44:53 -0400

    Component Composition Introduced

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-// React Functional Component
+const TitleComponent = () => (
+        <h1 className="head" >Title Component</h1> 
+)
+
 const HeadingComponent = () => (
     <div id="container">
+        <TitleComponent/>
         <h1 className="heading" >React Functional Component</h1>
     </div>    
 )
