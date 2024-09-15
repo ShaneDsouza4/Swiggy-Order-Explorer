660476cbb5cf163ff76663f990126425e1143747
ShaneDsouza4 <shane.json96@gmail.com>
2024-09-15 01:24:59 -0400

    useState Hook used to filter restaurant cards

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,35 @@
 import RestaturantCard from "./RestaurantCard";
 import resDataAPI from "../../utils/mockData";
+import { useState } from "react";
+
 
 const Body = () => {
+
+  // Local State Variable - Super powerful variable
+  const [restaurantList, setrestaurantList ] = useState(resDataAPI)
+
+  
   return (
     <div className="body">
-      <div className="search">Search</div>
-      <div className="res-container">
-        {resDataAPI.map((x) => (
-          <RestaturantCard key={x.info.id} resData={x} />
+
+      <div className="filter">
+        <button className="filter-btn" onClick={ ()=> { 
+              
+              const filteredEestaurantList = restaurantList.filter(
+                (x) => x.info.avgRating > 4.5
+              )
+              console.log(filteredEestaurantList)
+              setrestaurantList(filteredEestaurantList);
+          }} 
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+
+
+      <div className="res-container"> 
+        {restaurantList.map((x) => (
+          <RestaturantCard key={x.info?.id} resData={x} />
         ))}
       </div>
     </div>
