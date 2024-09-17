0290b71456b0f2ea68ba663cc8737117d9a6017c
ShaneDsouza4 <shane.json96@gmail.com>
2024-09-17 16:33:55 -0400

    Fetched Live Data using useEffect and set list using useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,28 @@
 import RestaturantCard from "./RestaurantCard";
 import resDataAPI from "../../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 const Body = () => {
 
   // Local State Variable - Super powerful variable
-  const [restaurantList, setrestaurantList ] = useState(resDataAPI)
+  const [restaurantList, setrestaurantList ] = useState([])
 
+  useEffect(()=>{
+    fetchData();
+  }, [])
   
+  const fetchData = async () => {
+
+      const data = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.960059122809971&lng=77.57337538383284&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      );
+      const json = await data.json();
+      //console.log("-->", json.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)   
+      setrestaurantList(json.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+
+  };
+
   return (
     <div className="body">
 
