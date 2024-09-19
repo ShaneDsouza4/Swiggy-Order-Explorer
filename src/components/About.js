a2a264e42238b44b1ea9006888c86321a85a28da
ShaneDsouza4 <shane.json96@gmail.com>
2024-09-19 17:24:10 -0400

    Add User Class Based Component

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,15 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = () => {
-    return (
-        <div>
-            <h1>About</h1>
-            <h2>Lorem Ipsum</h2>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>About</h1>
+      <h2>Lorem Ipsum</h2>
+      <User name="Seth" />
+      <UserClass name="Mike" location="Windosor" />
+    </div>
+  );
+};
 
 export default About;
