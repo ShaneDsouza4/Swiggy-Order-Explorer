4d5969062c6361936c790437d4406ba01d00a740
ShaneDsouza4 <shane.json96@gmail.com>
2024-09-19 19:06:23 -0400

    Converted About Functional Comp -> Class Based Comp

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,28 @@
+import React from "react";
 import User from "./User";
 import UserClass from "./UserClass";
 
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-      <h2>Lorem Ipsum</h2>
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    //console.log("Prent Constructor");
+  }
 
-      <UserClass name="Mike" location="Windosor" />
-    </div>
-  );
-};
+  render() {
+    //console.log("Parent Render");
+    return (
+      <div>
+        <h1>About Class Component</h1>
+        <h2>Lorem Ipsum</h2>
+
+        <UserClass name={"Mike (class)"} location={"Windsor (class)"} />
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    //console.log("Parent Component Did Mount");
+  }
+}
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,34 +4,40 @@ import { useState } from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+    //console.log(this.props.name + " Child Constructor");
     //console.log("Props: ", props);
 
     this.state = {
-      count: 0,
+      //count: 0,
     };
   }
 
   render() {
+    //console.log(this.props.name + " Child Render");
     const { name, location } = this.props;
-    const { count } = this.state;
+    //const { count } = this.state;
 
     return (
       <div className="user-card">
         <button
           onClick={() => {
             this.setState({
-              count: this.state.count + 1,
+              //count: this.state.count + 1,
             });
           }}
         >
           Increase Count
         </button>
-        <p>{count}</p>
+
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h4>Contact: @shane.json</h4>
       </div>
     );
+  }
+
+  componentDidMount() {
+    //console.log(this.props.name + " Child Component Did Mount");
   }
 }
 
