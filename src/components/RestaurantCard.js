405f79f6cf39d17b456270e273f6d4e582d85177
ShaneDsouza4 <shane.json96@gmail.com>
2024-10-05 21:05:01 -0400

    Color change

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -8,7 +8,7 @@ const RestaturantCard = (props) => {
   const { slaString } = resData?.info?.sla;
 
   return (
-    <div className="m-4 p-4 w-[250px] rounded-lg bg-[#f0f0f0] hover:bg-green-100">
+    <div className="m-4 p-4 w-[250px] rounded-lg bg-[#f0f0f0] hover:bg-red-100">
       <img
         className="rounded-lg"
         alt="res-logo"
